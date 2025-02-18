2b4b77ac7536a944cebd9348968c4e80641ad3e5
ChanHo_Park <93924210+Chanhohoho@users.noreply.github.com>
2025-02-19 00:12:48 +0900

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,15 @@
+from _Key.Key import upbit
+from pprint import pprint
+
+# pprint(upbit.get_balances())
+
+KRW = upbit.get_balance('KRW')
+
+# try:
+#     upbit.get_balance('KRW')
+# except:
+#     print("HI")
+
+print(KRW)
+
+# print(type(KRW))
