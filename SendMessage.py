376100165383d74c36fe54cedccc4a5c490577c6
ChanHo_Park <93924210+Chanhohoho@users.noreply.github.com>
2025-02-18 23:14:26 +0900

    Update SendMessage.py

--- SendMessage.py
+++ SendMessage.py
@@ -1,10 +1,10 @@
 import requests
 import datetime
-
-testServer = "DISCORD_WEBHOOK_URL"
+from _Key.Key import DISCORD_WEBHOOK_URL
 
 def SendMessage(msg):
     """디스코드 메세지 전송"""
     now = datetime.datetime.now()
     message = {"content": f"[{now.strftime('%Y-%m-%d %H:%M:%S')}] {str(msg)}"}
-    requests.post(testServer, data=message)
+    requests.post(DISCORD_WEBHOOK_URL, data=message)
+    
